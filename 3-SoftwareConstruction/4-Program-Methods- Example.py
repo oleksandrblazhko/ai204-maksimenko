c6a19f22197225dba0449d04df61261f4f2e6f9c
Chugaister <79446015+AndreyMaksimenko@users.noreply.github.com>
2022-11-03 13:46:10 +0300

    Update 4-Program-Methods- Example.py

--- 3-SoftwareConstruction/4-Program-Methods- Example.py
+++ 3-SoftwareConstruction/4-Program-Methods- Example.py
@@ -1,23 +1,31 @@
+# Створення об'єктного типу (клас) Obj
 class Obj:
     def __init__(self):
+        # Якість повітря та температура
         self.x = random.randint(0, 100)
-        
+
+
+# Створення об'єктного типу (клас) Environment
 class Environment:
     def __init__(self, air_quality, temperature):
-        self.air_quality = air_quality
-        self.temperature = temperature
+        self.air_quality = air_quality  # якість повітря
+        self.temperature = temperature  # температура повітря
 
+    # Пошук інформації про якість повітря
     def info_air(self):
         self.air_quality = Obj()
         return self.air_quality
 
+    # Пошук інформації про температуру повітря
     def info_temperature(self):
         self.temperature = Obj()
         return self.temperature
 
+    # Місцезнаходження користувача
     def information_sharing(self):
         return print(self.consumer_location)
 
+    # Відправка загальної інформації про якість повітря та температуру поітря за місцезнаходженням користувача
     def send_info(self):
         return print(
             "AIR: " + self.air_quality + "% " + "Temperature: " + self.temperature + "% on " + self.consumer_location)
