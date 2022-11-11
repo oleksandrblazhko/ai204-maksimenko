fcb489a6a5819805a887de74ebfd22359edd0a5c
Chugaister <79446015+AndreyMaksimenko@users.noreply.github.com>
2022-11-11 20:54:36 +0300

    Update 4-Program-Methods- Example.py

--- 3-SoftwareConstruction/4-Program-Methods- Example.py
+++ 3-SoftwareConstruction/4-Program-Methods- Example.py
@@ -1,31 +1,39 @@
-# Створення об'єктного типу (клас) Obj
-class Obj:
+# Створення об'єктного типу (клас) Loc
+class Loc:
     def __init__(self):
-        # Якість повітря та температура
-        self.x = random.randint(0, 100)
+        # Координати
+        self.x = random.randint(-2070, 2070)
 
 
-# Створення об'єктного типу (клас) Environment
-class Environment:
-    def __init__(self, air_quality, temperature):
-        self.air_quality = air_quality  # якість повітря
-        self.temperature = temperature  # температура повітря
+class Area:
+    def __init__(self):
+        # Радіус пошуку
+        self.x = random.randint(1, 5)
 
-    # Пошук інформації про якість повітря
-    def info_air(self):
-        self.air_quality = Obj()
-        return self.air_quality
 
-    # Пошук інформації про температуру повітря
-    def info_temperature(self):
-        self.temperature = Obj()
-        return self.temperature
+# Створення об'єктного типу (клас) Location
+class Location:
+    def __init__(self, xCoord, yCoord, area):
+        self.consumer_location = consumer_location
+        self.xCoord = xCoord  # позиция по плоскости х
+        self.yCoord = yCoord  # позиция по плоскости y
+        self.area = area  # радіус пошуку
 
-    # Місцезнаходження користувача
-    def information_sharing(self):
-        return print(self.consumer_location)
+    def location_sharing(self):
+        xCoord = Loc()
+        self.xCoord = xCoord
+        yCoord = Loc()
+        self.yCoord = yCoord
+        area = Area()
+        self.area = area
+        if self.xCoord == "":
+            print("Результат = -1")
+        elif self.area == "":
+            print("Результат = -2")
+        elif 1 > self.area > 5:
+            print("Результат = -2")
+        elif self.yCoord == "" and self.area == "":
+            print("Результат = -3")
+        else:
+            return print("Результат = 1;", xCoord, yCoord, area)
 
-    # Відправка загальної інформації про якість повітря та температуру поітря за місцезнаходженням користувача
-    def send_info(self):
-        return print(
-            "AIR: " + self.air_quality + "% " + "Temperature: " + self.temperature + "% on " + self.consumer_location)
