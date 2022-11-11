6b9fb5388c0f87c71630e101c34617c5c3b5f32f
Chugaister <79446015+AndreyMaksimenko@users.noreply.github.com>
2022-11-11 20:53:42 +0300

    Update 3-Program-Сlasses-Example.py

--- 3-SoftwareConstruction/3-Program-Сlasses-Example.py
+++ 3-SoftwareConstruction/3-Program-Сlasses-Example.py
@@ -19,20 +19,37 @@ class Loc:
         # Координати
         self.x = random.randint(-2070, 2070)
 
+class Area:
+    def __init__(self):
+        # Радіус пошуку
+        self.x = random.randint(1, 5)
+
 
 # Створення об'єктного типу (клас) Location
 class Location:
-    def __init__(self, xCoord, yCoord):
+    def __init__(self, xCoord, yCoord, area):
         self.consumer_location = consumer_location
         self.xCoord = xCoord  # позиция по плоскости х
         self.yCoord = yCoord  # позиция по плоскости y
+        self.area = area  # радіус пошуку
 
     def location_sharing(self):
         xCoord = Loc()
         self.xCoord = xCoord
         yCoord = Loc()
         self.yCoord = yCoord
-        return print(self.consumer_location, self.xCoord, self.yCoord)
+        area = Area()
+        self.area = area
+        if self.xCoord == "":
+            print("Результат = -1")
+        elif self.area == "":
+            print("Результат = -2")
+        elif 1 > self.area > 5:
+            print("Результат = -2")
+        elif self.yCoord == "" and self.area == "":
+            print("Результат = -3")
+        else:
+            return print("Результат = 1;", xCoord, yCoord, area)
 
 
 # Створення об'єктного типу (клас) Obj
